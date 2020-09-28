018fc4763649c6a2ce9e90a0b068318bc39bcaa5
Krin <lightdark@bk.ru>
2020-09-28 14:09:59 +0300

    linter

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function getRandomInt(min, max) {
   return Math.floor(min + Math.random() * Math.floor(max - min));
 }
@@ -7,11 +9,11 @@ map.classList.remove("map--faded");
 
 function createOffers(count = 8) {
 
-  const offer_types = ["palace", "flat", "house", "bungalow"];
-  const checkin_times = ["12:00", "13:00", "14:00"];
-  const checkout_times = ["12:00", "13:00", "14:00"];
+  const offerTypes = ["palace", "flat", "house", "bungalow"];
+  const checkinTimes = ["12:00", "13:00", "14:00"];
+  const checkoutTimes = ["12:00", "13:00", "14:00"];
   const features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-  const offer_photos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+  const offerPhotos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
   const offers = [];
   let locationX;
   let locationY;
@@ -21,7 +23,7 @@ function createOffers(count = 8) {
     locationX = getRandomInt(0, map.clientWidth);
     locationY = getRandomInt(130, 630);
 
-    offers[i-1] = {
+    offers[i - 1] = {
       "author": {
         "avatar": "img/avatars/user0" + (i) + ".png",
       },
@@ -29,21 +31,22 @@ function createOffers(count = 8) {
         "title": "Заголовок " + i,
         "address": locationX + ", " + locationY,
         "price": getRandomInt(1000, 3000),
-        "type": offer_types[getRandomInt(0, offer_types.length)],
+        "type": offerTypes[getRandomInt(0, offerTypes.length)],
         "rooms": getRandomInt(1, 3),
         "guests": getRandomInt(1, 3),
-        "checkin": checkin_times[getRandomInt(0, checkin_times.length)],
-        "checkout": checkout_times[getRandomInt(0, checkout_times.length)],
+        "checkin": checkinTimes[getRandomInt(0, checkinTimes.length)],
+        "checkout": checkoutTimes[getRandomInt(0, checkoutTimes.length)],
         "features": features,
         "description": "Описанием " + i,
-        "photos": offer_photos,
+        "photos": offerPhotos,
       },
       "location": {
         "x": locationX,
         "y": locationY,
-      }
-    }
+      },
+    };
   }
+
   return offers;
 }
 
@@ -52,14 +55,14 @@ const mapPinTemplate = document.querySelector("#pin");
 function renderPin(offer) {
   let mapPin = mapPinTemplate.cloneNode(true).content;
 
-  mapPin.querySelector('.map__pin').style = "left: " + offer.location.x + "px; top: " + offer.location.y + "px;";
-  mapPin.querySelector('img').src = offer.author.avatar;
-  mapPin.querySelector('img').alt = offer.offer.title;
+  mapPin.querySelector(".map__pin").style = "left: " + offer.location.x + "px; top: " + offer.location.y + "px;";
+  mapPin.querySelector("img").src = offer.author.avatar;
+  mapPin.querySelector("img").alt = offer.offer.title;
   return mapPin;
 }
 
 function createDom(offers) {
-  let pinList = map.querySelector('.map__pins');
+  let pinList = map.querySelector(".map__pins");
   let fragment = document.createDocumentFragment();
 
   for (let i = 0; i < offers.length; i++) {

f3db8fad4605328a415445daca802b8f9df1ad4d
Krin <lightdark@bk.ru>
2020-10-04 00:14:41 +0300

    3 часть ДЗ

--- js/main.js
+++ js/main.js
@@ -240,3 +240,37 @@ function compareFields() {
 
 roomNumber.addEventListener("input", compareFields);
 capacity.addEventListener("input", compareFields);
+
+
+let typeField = document.querySelector("#type");
+let priceField = document.querySelector("#price");
+
+typeField.addEventListener("input", evt => {
+  switch (evt.target.options.selectedIndex){
+    case 0:
+      priceField.setAttribute("min", "0");
+      break;
+    case 1:
+      priceField.setAttribute("min", "1000");
+      break;
+    case 2:
+      priceField.setAttribute("min", "5000");
+      break;
+    case 3:
+      priceField.setAttribute("min", "10000");
+      break;
+    default:
+      priceField.setAttribute("min", "0");
+  };
+});
+
+let timeinField = document.querySelector("#timein");
+let timeoutField = document.querySelector("#timeout");
+
+timeinField.addEventListener("input", evt => {
+  timeoutField.options.selectedIndex = evt.target.options.selectedIndex;
+});
+
+timeoutField.addEventListener("input", evt => {
+  timeinField.options.selectedIndex = evt.target.options.selectedIndex;
+});

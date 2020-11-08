ce3cd32857a7c46d150fb5a613cb426cd9f315f8
Krin <lightdark@bk.ru>
2020-11-08 21:27:01 +0300

    Закрытие карточки обьявления при сбросе страницы

--- js/page.js
+++ js/page.js
@@ -4,6 +4,7 @@ const resetPage = () => {
   const DEFAULT_PIN_X = 570;
   const DEFAULT_PIN_Y = 375;
   window.form.reset();
+  window.pin.closeCard();
   window.map.clearPins();
   window.map.setNonActiveState();
   window.mainPin.moveMainPin(DEFAULT_PIN_X, DEFAULT_PIN_Y);
